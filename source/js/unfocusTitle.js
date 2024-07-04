6b3c6518ba7db2e22a6f1ca46d560dac76b3cb08
SakuraKooi <me@sakurakooi.dev>
2024-07-04 15:00:52 +0800

    feat: add unfocus title

---
+++ source/js/unfocusTitle.js
@@ -0,0 +1,12 @@
+(() => {
+    let oldTitle;
+    document.addEventListener("visibilitychange", function () {
+            if (document.hidden) {
+                oldTitle = document.title;
+                document.title = "诶!? 不许跑喵~";
+            } else {
+                document.title = oldTitle;
+            }
+        }
+    );
+})();
