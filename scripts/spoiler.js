7b18c88d3ae191c45776cda11cf886e7b1bbe628
SakuraKooi <me@sakurakooi.dev>
2024-07-04 15:00:55 +0800

    feat: add spoiler

---
+++ scripts/spoiler.js
@@ -0,0 +1,21 @@
+// Modified from https://github.com/fletchto99/hexo-sliding-spoiler to fit theme style
+hexo.extend.tag.register('spoiler', (args, content) =>
+    `<div class='spoiler collapsed'>
+    <div class='spoiler-title'>
+        <span>
+            <i class="fa-solid fa-bars-staggered"></i>
+            ${args.join(" ")}
+        </span>
+        <i class="spoiler-icon fa-solid fa-angles-down"></i>
+    </div>
+    <div class='spoiler-content'>
+        ${
+        hexo.render.renderSync({
+            text: content,
+            engine: "markdown"
+        }) || ""
+    }
+    </div>
+</div>`, {
+    ends: true
+});
--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,3 @@
 import('./focus-title.js')
 import("./navigation-utility.js")
+import("./plugins/spoiler.js")
