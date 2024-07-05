07082dc5e4bd837fb5bb71b07faf7de56bdcbe9a
SakuraKooi <me@sakurakooi.dev>
2024-07-05 14:03:09 +0800

    fix: undefined title

--- source/js/focus-title.js
+++ source/js/focus-title.js
@@ -1,5 +1,5 @@
 (() => {
-    let oldTitle;
+    let oldTitle = document.title;
     document.addEventListener("visibilitychange", function () {
             if (document.hidden) {
                 oldTitle = document.title;
