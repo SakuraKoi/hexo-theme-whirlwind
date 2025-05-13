2b407b07541846f567250db069f52b62405690f0
SakuraKooi <me@sakurakooi.dev>
2025-05-13 13:35:06 +0800

    refactor: reorder script to ensure dark mode applies ASAP

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,4 @@
-import('./focus-title.js')
-import("./navigation-utility.js")
 import ('./dark-mode.js')
+import("./navigation-utility.js")
 import("./plugins/spoiler.js")
+import('./focus-title.js')
